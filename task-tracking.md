1e1f22352bd0588c9774655e515e2433b6ab4879
Garry Peffer <gpeffer98@gmail.com>
2021-11-04 16:24:31 -0400

    Working on changes

---
+++ task-tracking.md
@@ -0,0 +1,3 @@
+Take out the TrashWash the dishesWash the dogFigure out how to use Pthonlkajdsf
+asdf
+asdfdsf
---
+++ task-tracking.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+f = open("/home/garrypeffer/Python/task-tracking.md", "r")
+print(f.read())
+
+print(str('\nOptions: (e)ntry, (f)inish, (q)uit'))
+
+action = input("Enter option: \n\n")
+
+
+if action == str('e') or action == str('entry'):
+    entry = input("Enter entry: \n")
+    f = open("/home/garrypeffer/Python/task-tracking.md", "a")
+    f.write("\n" + (entry) + "\n")
+    f.close()
+#if action == str('f') or action == str('finish'):
+if action == str('q') or action == str('quit'):
+    print(str('Have a nice day!'))
+    break
